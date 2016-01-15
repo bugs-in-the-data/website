e6b950158e61ba51f6021a75b39c112599383d1e
Ty Skelton <skeltont@onid.oregonstate.edu>
2016-01-14 16:13:38 -0800

    adds graph template with placeholder graphs to main template.

--- app/models/species.py
+++ app/models/species.py
@@ -2,8 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
-class Species(models.Model):
+class SpeciesModel(models.Model):
     sample_id = models.CharField(max_length=150)
     state = models.CharField(max_length=150)
     drainage = models.CharField(max_length=150)
+
+    def getData(self):
+        test = {
+            'name': 'Ty'
+        }
+        return test
