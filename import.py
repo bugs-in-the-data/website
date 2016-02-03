b23681835bdc0b34ee1eea776f35a1367ca9d829
Ty Skelton <skeltont@onid.oregonstate.edu>
2016-02-02 18:09:48 -0800

    reads out necessary site info

--- import.py
+++ import.py
@@ -1,10 +1,11 @@
 ##
 # Install numpy and pandas!!
 #
-# 
+#
 ##
 import numpy as np
 import pandas as pd
+import MySQLdb
 
 
 def load_data_from_csv(filename):
@@ -13,12 +14,27 @@ def load_data_from_csv(filename):
 
 
 def main():
+	conn = MySQLdb.connect(
+				host = "localhost",
+				user = "root",
+				passwd = "Temp123",
+				db="bugdata"
+			)
+	x = conn.cursor()
+
 	# Load data into memory
 	och = load_data_from_csv('../data/och.csv')
 	habitat = load_data_from_csv('../data/habitat.csv')
 	# Example usage of numpy arrays:
-	for row in och:
-		print row['Order'], row['Family']
+	# for row in och:
+	# 	print row['Order'], row['Family']
+	for row in habitat:
+		# x.execute("""INSERT INTO app_sitemodel VALUES ())
+		print "State: " + row['State:Installation'].split(':')[0].strip()
+		print "Installation: " + row['State:Installation'].split(':')[1].strip()
+		print "Drainage: " + str(row['Drainage']).strip()
+		print "Mountain Range: " + row['MtnRange'].strip()
+		print " "
 
 if __name__ == '__main__':
 	main()
